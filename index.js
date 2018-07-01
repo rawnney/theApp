34d0bc1ff63fc8768efec07c54fbb518de474a2d
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2018-07-01 16:11:55 +0200

    json, eslint, flow

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// @flow
+import {AppRegistry} from 'react-native'
+import App from './components/App'
 
-AppRegistry.registerComponent('theApp', () => App);
+AppRegistry.registerComponent('theApp', () => App)
