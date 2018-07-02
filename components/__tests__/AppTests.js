e4b7d7f4be4aa4944a3183e9bac49bc3c264a5fe
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2018-07-02 22:09:06 +0200

    add test

---
+++ components/__tests__/AppTests.js
@@ -0,0 +1,15 @@
+import 'react-native'
+import React from 'react'
+import App from '../App'
+import renderer from 'react-test-renderer'
+
+describe('App', () => {
+  it('should be able to run tests', () => {
+    expect(1 + 2).toEqual(3)
+  })
+  it('renders correctly', () => {
+	// eslint-disable-next-line
+	  const tree = renderer.create(
+  <App />)
+  })
+})
