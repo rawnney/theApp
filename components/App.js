3826ab05959aade3b99c183905295897eeffa2d4
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2018-07-06 22:41:00 +0200

    add HomeView

--- components/App.js
+++ components/App.js
@@ -1,30 +1,9 @@
 // @flow
 import React, {Component} from 'react'
-import {StyleSheet, Text, View} from 'react-native'
-import Colors from '../libs/Colors'
+import HomeView from './HomeView'
 
 export default class App extends Component<*> {
   render (): React$Element<*> {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to TheApp!
-        </Text>
-      </View>
-    )
+    return <HomeView />
   }
 }
-
-let styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: Colors.blue
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  }
-})
---
+++ components/HomeView.js
@@ -0,0 +1,30 @@
+// @flow
+import React, {Component} from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+import Colors from '../libs/Colors'
+
+export default class HomeView extends Component<*> {
+  render (): React$Element<*> {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to TheApp!
+        </Text>
+      </View>
+    )
+  }
+}
+
+let styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.blue
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  }
+})
