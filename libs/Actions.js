0b18b26f561c440479cca880e0a82b3f9ee6fae1
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2018-07-07 11:41:35 +0200

    Add Actions

--- libs/Actions.js
+++ libs/Actions.js
@@ -1,2 +1,16 @@
 // @flow
+import {NavigationActions} from 'react-navigation'
+
+let {RESET} = NavigationActions
+
+// Navigation
+export let goBack = () => ({type: 'Navigation/BACK'})
+export let popTo = (component: *) => ({type: RESET, component})
+export let popToTop = popTo
+export let login = (user: User) => ({type: 'LOGIN', user})
+export let logout = () => ({type: 'LOGOUT'})
+
+// Redux
 export let updateUser = (user: User) => ({type: 'UPDATE_USER', user})
+export let updateState = (state: Object) => ({type: 'UPDATE_STATE', state})
+export let updateNavigationState = (navigationState: Object) => ({type: 'updateNavigationState', navigationState})
