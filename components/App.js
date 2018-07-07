b5b89ab679b9190dcbe1babdca3cecf70ad2f1b0
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2018-07-07 22:46:33 +0200

    add jest-cli, include provider

--- components/App.js
+++ components/App.js
@@ -1,15 +1,16 @@
 // @flow
 import React, {Component} from 'react'
-// import {Provider} from 'react-redux'
-// import {createStore} from '../libs/Store'
+import {Provider} from 'react-redux'
+import {createStore} from '../libs/Store'
 import {createStackNavigator} from 'react-navigation'
 import routes from '../libs/Routes'
 
 const AppNavigator = createStackNavigator(routes)
-// <Provider store={createStore()}>
-// </Provider>
+
 export default class App extends Component<*> {
   render (): * {
-    return <AppNavigator />
+    return <Provider store={createStore()}>
+      <AppNavigator />
+    </Provider>
   }
 }
