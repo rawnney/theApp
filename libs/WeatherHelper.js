122646a439c9dfcf44cf082810f8c11978448883
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2018-08-02 23:31:06 +0200

    update weatherHelper

--- libs/WeatherHelper.js
+++ libs/WeatherHelper.js
@@ -13,3 +13,35 @@ export let getWeather = (position: Object, fixedLat?, fixedLng?): Promise <Objec
       })
   })
 }
+
+export let getWeatherTips = (weather: Object): string => {
+  let tipsArray = []
+  let goodTips = goodTipsArray()
+  let tipName = getTipName(weather)
+  tipsArray.push(goodTips.find(obj => obj.name === tipName).value)
+  let randomize = Math.floor(Math.random() * tipsArray.length)
+  let tip = tipsArray[randomize]
+  return tip
+}
+
+let getTipName = (weather: Object) => {
+  let {temp, humidity} = weather.main
+  let {speed} = weather.wind
+  switch (true) {
+    case temp > '25': return 'highTemp'
+    case temp < '16': return 'lowTemp'
+    case speed > '6': return 'highWind'
+    case humidity > '80': return 'humid'
+    default: return 'default'
+  }
+}
+
+let goodTipsArray = () => {
+  return [
+    {name: 'highTemp', value: '...superhot outside today! 🔥'},
+    {name: 'lowTemp', value: 'Don\'t forget the sweater! ⛄️'},
+    {name: 'highWind', value: 'Time to go sailing? ⛵️'},
+    {name: 'humid', value: 'So moist... 💦 '},
+    {name: 'default', value: 'Stay rad! 🤙'}
+  ]
+}
