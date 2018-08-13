57ab8716e89c5f2cf785e2ede9dee3f48df6fdd1
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2018-08-13 03:30:23 +0200

    testablecomps

--- test/TestableComponents.js
+++ test/TestableComponents.js
@@ -13,8 +13,20 @@ var testableComponents = [
     propsArray: [{disable: true, wrapperStyle: {}, buttonStyle: {}, onPress: jest.fn()}]
   },
   {
-    key: '../components/Test',
-    propsArray: [{}]
+    key: '../components/CrimeExtendedContainer',
+    propsArray: [{crime: {}, navigation: {}}]
+  },
+  {
+    key: '../components/CrimeListItem',
+    propsArray: [{onPress: jest.fn(), id: 'string', type: 'string', date: 'string', lat: 'string', lng: 'string', location: 'string', content: 'string', description: 'string'}]
+  },
+  {
+    key: '../components/CrimeContainer',
+    propsArray: [{navigation: {}}]
+  },
+  {
+    key: '../components/CrimeView',
+    propsArray: [{crimes: {}, onPressCrime: jest.fn()}]
   }
 ]
 
