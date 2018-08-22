83d281ccef9aff81d49ab76e62e63801d3f60aa7
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2018-08-22 17:11:36 +0200

    link missing packages

--- android/app/src/main/java/com/theapp/MainApplication.java
+++ android/app/src/main/java/com/theapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.theapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.sensors.RNSensorsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new ReactNativeConfigPackage(),
             new RNSensorsPackage(),
             new RNDeviceInfo()
