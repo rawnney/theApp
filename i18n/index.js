d8ff6d2e862c1893aae495fb4de9a33c61f50cc4
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2018-08-23 10:24:53 +0200

    i18n index update

--- i18n/index.js
+++ i18n/index.js
@@ -1,14 +1,15 @@
 // @flow
-// import I18n from 'react-native-i18n' // NOTE: NEEDED OR...?
 import sv from './text_strings/client/sv.json'
 import en from './text_strings/client/en.json'
 import _default from './text_strings/client/default.json'
+// import I18n from 'react-native-i18n' // NOTE: NEEDED OR...?
 
 export var supportedLanguageCodes = ['sv', 'en']
 
 export let getTextStrings = (lang: string) => {
   switch (lang) {
-    case 'sv' : return {..._default, ...sv}
-    default : return {..._default, ...en}
+    case 'sv': return {..._default, ...sv}
+    case 'en': return {..._default, ...sv}
+    default: return {..._default, ...en}
   }
 }
