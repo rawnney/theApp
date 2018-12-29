a69ad446d6f23d274a871b4fe02579dccf5744b9
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2018-12-29 23:05:57 +0800

    arrows

--- libs/ColorThemeHelper.js
+++ libs/ColorThemeHelper.js
@@ -1,12 +1,12 @@
 // @flow
 import Store from '../libs/Store'
 
-export function primaryColor (): string { return Store.getState().user.colorTheme.primary }
-export function secondaryColor (): string { return Store.getState().user.colorTheme.secondary }
-export function primaryTextColor (): string { return Store.getState().user.colorTheme.primaryText }
-export function secondaryTextColor (): string { return Store.getState().user.colorTheme.secondaryText }
-export function warningColor (): string { return Store.getState().user.colorTheme.warning }
-export function borderColor (): string { return Store.getState().user.colorTheme.secondaryText }
-export function headerBarTint (): string { return Store.getState().user.colorTheme.headerBarTint }
-export function keyBoardAppearance (): string { return Store.getState().user.colorTheme.keyboardAppearance }
-export function disabledColor (): string { return Store.getState().user.colorTheme.disabled }
+export let primaryColor = (): string => Store.getState().user.colorTheme.primary
+export let secondaryColor = (): string => Store.getState().user.colorTheme.secondary
+export let primaryTextColor = (): string => Store.getState().user.colorTheme.primaryText
+export let secondaryTextColor = (): string => Store.getState().user.colorTheme.secondaryText
+export let warningColor = (): string => Store.getState().user.colorTheme.warning
+export let borderColor = (): string => Store.getState().user.colorTheme.secondaryText
+export let headerBarTint = (): string => Store.getState().user.colorTheme.headerBarTint
+export let keyBoardAppearance = (): string => Store.getState().user.colorTheme.keyboardAppearance
+export let disabledColor = (): string => Store.getState().user.colorTheme.disabled
